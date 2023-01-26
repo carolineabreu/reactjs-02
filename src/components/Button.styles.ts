4e4d04523a788bed6350890e1cf5afa0de0ed7f7
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-01-26 10:57:15 -0300

    styled components

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,49 @@
+/* Como seria fazer várias cores pro mesmo botão no css puro, no arquivo 
+Button.module.css:
+.button {
+  width: 100px;
+  height: 40px;
+}
+
+.primary {
+  background: purple;
+}
+
+.secondary {
+  background: orange;
+}
+
+.danger {
+  background: red;
+}
+
+.success {
+  background: green;
+} */
+
+// a mesma coisa agora com o styled components:
+import styled, { css } from 'styled-components';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  ${props => {
+    return css`
+      background-color: ${buttonVariants[props.variant]} 
+    `
+  }}
+`
