ce61ee8ff299d81571d6f0af166eab66bc062cac
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-01-30 16:17:13 -0300

    :lipstick: feat: add font family and default colors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,8 +7,19 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['green-300']};
+  }
+
   body {
-    background: #333;
-    color: #fff
+    background: ${props => props.theme['gray-900']}; //não da pra usar a dot notation: theme.gray-900 porque tem o -, por isso tem que colocar []
+    color: ${props => props.theme['gray-300']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
   }
 `;
